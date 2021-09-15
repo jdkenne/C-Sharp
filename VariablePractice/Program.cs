53960b97e1099c698497bdd305fc2ae8a3682031
jdkenne <jdkenne@g.clemson.edu>
2021-09-15 13:01:07 -0400

    Variable Practice added

---
+++ VariablePractice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VariablesTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ourNumber;
+            ourNumber = 0;
+            Console.WriteLine(ourNumber);
+            
+            ourNumber = 10;
+            Console.WriteLine(ourNumber);
+
+            ourNumber = 2 + 2;
+            Console.WriteLine(ourNumber);
+
+            double decimalNumber;
+            decimalNumber = 10.15;
+            Console.WriteLine(decimalNumber);
+            
+            int number = 5;
+            Console.WriteLine(number);
+
+            char myCharacter = 'c';
+            Console.WriteLine(myCharacter);
+
+            string myName = "Joshua";
+            Console.WriteLine(myName);
+            
+            int newVariable = ourNumber - number;
+            Console.WriteLine(newVariable);
+
+            Boolean temp = true;
+            Console.WriteLine(temp);
+        }
+    }
+}
